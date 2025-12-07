1ed950486cfac1212cea4d8187c91e1485c9453f
Michael Harp <mike@mikeharp.com>
2025-12-07 08:48:52 -0500

    Fix puppet-lint errors in base profile

--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,17 +1,30 @@
 # The base profile should include component modules that will be on all nodes
-class profile::base {
-  notify { 'Base profile applied': }
+class profile::base (
+  Integer $release,
+) {
   include chrony
   include firewall
 
   firewall { '100 allow puppet':
-    dport  => 8140,
-    proto  => 'tcp',
-    jump   => 'ACCEPT',
+    dport => 8140,
+    proto => 'tcp',
+    jump  => 'ACCEPT',
   }
 
   class { 'sudo': }
   sudo::conf { 'mharp':
-    content  => 'mharp ALL=(ALL) NOPASSWD: ALL',
+    content => 'mharp ALL=(ALL) NOPASSWD: ALL',
+  }
+
+  $os_name = $facts['os']['name'] ? {
+    'Fedora' => 'fedora',
+    default  => 'el',
+  }
+  $url = "https://yum.voxpupuli.org/openvox${release}-release-${os_name}-${facts['os']['release']['major']}.noarch.rpm"
+
+  package { "openvox${release}-release":
+    ensure   => present,
+    provider => 'rpm',
+    source   => $url,
   }
 }
