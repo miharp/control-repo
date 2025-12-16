6117500099752630bab125c2e3bc88ddf7849775
Michael Harp <mike@mikeharp.com>
2025-12-16 08:35:42 -0500

    Fix OpenVox agent versioning and repo metadata expiration

--- site-modules/profile/manifests/openvox_agent.pp
+++ site-modules/profile/manifests/openvox_agent.pp
@@ -10,7 +10,15 @@
 class profile::openvox_agent (
   String $version,
 ) {
+  if $facts['os']['family'] == 'RedHat' {
+    $package_version = "${version}-1.el${facts['os']['release']['major']}"
+  } elsif $facts['os']['name'] == 'Ubuntu' {
+    $package_version = "${version}-1+ubuntu${facts['os']['release']['full']}"
+  } else {
+    $package_version = $version
+  }
+
   package { 'openvox-agent':
-    ensure => $version,
+    ensure => $package_version,
   }
 }
