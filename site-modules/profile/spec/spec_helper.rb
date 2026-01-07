42cf4fb2148a39527180da25e4c8d9e6cac6b670
Michael Harp <mike@mikeharp.com>
2026-01-07 08:25:53 -0500

    Fix profile specs in VoxBox
    Drop puppetlabs_spec_helper dependency, configure rspec-puppet fixtures, and pin logger gem to avoid Rubygems ambiguity warnings.

--- site-modules/profile/spec/spec_helper.rb
+++ site-modules/profile/spec/spec_helper.rb
@@ -1,10 +1,20 @@
 # frozen_string_literal: true
 
-require 'puppetlabs_spec_helper/module_spec_helper'
+begin
+  gem 'logger', '~> 1.7'
+rescue Gem::LoadError
+  # Fall back to the default logger if this version isn't available.
+end
+require 'logger'
+
+require 'rspec-puppet'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
+  fixture_path = File.expand_path('fixtures', __dir__)
+  c.module_path = File.join(fixture_path, 'modules')
+
   c.default_facts = {
     os: {
       family: 'RedHat',
